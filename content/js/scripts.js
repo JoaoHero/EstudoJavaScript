61245c529ce8c324784a32d267d97b88b63588b9
João Ramos <Joao.silvaramos2013@hotmail.com>
2023-07-26 15:28:15 -0300

    Criado a funcao de desconto em script.js

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -1,4 +1,5 @@
 const showAllButton = document.querySelector(".show-all");
+const mapButton = document.querySelector(".map-all");
 const list = document.querySelector(".list");
 
 function showAll(products) {
@@ -18,6 +19,18 @@ function showAll(products) {
     list.innerHTML = newLi;
 };
 
+function mapAll() {
+
+    const newPrice = menuOptions.map(element => ({
+        ...element,
+        price: element.price * 0.9
+    }));
+
+    showAll(newPrice);
+};
+
 
 
 showAllButton.addEventListener('click', () => showAll(menuOptions));
+
+mapButton.addEventListener('click', mapAll);
